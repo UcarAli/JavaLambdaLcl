bc44541b726c5efa342cefea19347dd18a510ba0
M. Ali UCAR <ucaralifr@gmail.com>
2022-04-30 00:57:16 +0200

    22/04-29

--- src/lambdaTutorial/Lambda02.java
+++ src/lambdaTutorial/Lambda02.java
@@ -1,189 +1,188 @@
 package lambdaTutorial;
 
-import lambdaTutorial.Lambda01;
-
 import java.util.*;
 
 public class Lambda02 {
     public static void main(String[] args) {
 
         List<Integer> sayi = new ArrayList<>(Arrays.asList(4, 2, 6, 11, -5, 7, 3, 15));
-        ciftKarePrint(sayi);//16 4 36
-        System.out.println("\n   ***   ");
-        tekKupBirFazlaPrint(sayi);//1332 -124 344 28 3376
-        System.out.println("\n   ***   ");
-        ciftKarekokPrint(sayi);
-        System.out.println("\n   ***   ");
-        maxElBul(sayi);
-        System.out.println("\n   ***   ");
-        ciftKarteMaxBul(sayi);
-        System.out.println("\n   ***   ");
-        elTopla(sayi);
-        System.out.println("\n   ***   ");
+
+        System.out.println("   *** ciftKarePrint() ***   ");
+        ciftKarePrint(sayi); //1156 484 256 400 64 1444
+        System.out.println("\n\n   *** tekKupBirFazlaPrint() ***   ");
+        tekKupBirFazlaPrint(sayi);
+        System.out.println("\n\n   *** ciftKareKokPrint() ***   ");
+        ciftKareKokPrint(sayi);
+        System.out.println("\n\n   *** maxElmBul() ***   ");
+        maxElmBul(sayi);
+        System.out.println("\n\n   *** ciftKareMaxBul() ***   ");
+        ciftKareMaxBul(sayi);
+        System.out.println("\n\n   *** elmTopla() ***   ");
+        elmTopla(sayi);
+        System.out.println("\n\n   *** ciftCarp() ***   ");
         ciftCarp(sayi);
-        System.out.println("\n   ***   ");
+        System.out.println("\n\n   *** minBul() ***   ");
         minBul(sayi);
-        System.out.println("\n   ***   ");
+        System.out.println("\n\n   *** bestenBykEnKck() ***   ");
         bestenBykEnKck(sayi);
-        System.out.println("\n   ***   ");
-        tekKareBkPrint( sayi);
-        System.out.println("\n   ***   ");
-        ciftKareKbPrint(sayi);
-        System.out.println("\n   ***   ");
+        System.out.println("\n\n   *** ciftKareKcktenBygePrint() ***   ");
+        ciftKareKcktenBygePrint(sayi);
+        System.out.println("\n\n   *** tekKareByktenKcgePrint() ***   ");
+        tekKareByktenKcgePrint(sayi);
 
 
 
     }
 
-    // Task : Functional Programming ile listin cift elemanlarinin  karelerini ayni satirda aralarina bosluk bırakarak print ediniz
-
+    // Task : Functional Programming ile listin cift elemanlarinin  karelerini ayni satirda aralarina bosluk bırakarak print ediniz.
     public static void ciftKarePrint(List<Integer> sayi) {
         sayi.
                 stream().
                 filter(Lambda01::ciftBul).
-                map(t -> t * t).//map()--> Stream içerisindeki elemanları başka tiplere dönüştürmek veya üzerlerinde işlem yapmak (update) için Map kullanılmaktadır.
+                map(t->t*t). // map()--> Stream içerisindeki elemanları başka tiplere dönüştürmek veya üzerlerinde işlem yapmak (update) için Map kullanılmaktadır.
                 forEach(Lambda01::yazdir);
 
-
     }
-    // Task : Functional Programming ile listin tek elemanlarinin  kuplerinin bir fazlasini ayni satirda aralarina bosluk birakarak print edi
 
+    // Task : Functional Programming ile listin tek elemanlarinin  kuplerinin bir fazlasini ayni satirda aralarina bosluk birakarak print ediniz.
     public static void tekKupBirFazlaPrint(List<Integer> sayi) {
         sayi.
-                stream().//sayilar akısa  alındı
-                filter(t -> t % 2 != 0).//tek elemnalr fitrelendi
-                map(t -> (t * t * t) + 1).//tek elemanları küplerinin 1 fazlasına udate edildi
-                forEach(Lambda01::yazdir);//print edildi
-
+                stream(). // sayilar akisa alindi
+                filter(t-> t%2!=0).
+                map(t-> (t*t*t)+1). // tek elemanlar kuplerinin 1 fazlasina update edildi
+                forEach(Lambda01::yazdir);
     }
 
-    // Task : Functional Programming ile listin cift elemanlarinin   karekoklerini ayni satirda aralarina bosluk birakarak yazdiriniz
-    public static void ciftKarekokPrint(List<Integer> sayi) {
+    // Task : Functional Programming ile listin cift elemanlarinin   karekoklerini ayni satirda aralarina bosluk birakarak yazdiriniz.
+    public static void ciftKareKokPrint(List<Integer> sayi) {
         sayi.
                 stream().
                 filter(Lambda01::ciftBul).
-                map(Math::sqrt).//meth ref--> double deger return eder
-                //forEach(lambdaTutorial.Lambda01::yazdir)--> yazdir() method int.  çalıştığı için map den çıkan dataları çalıştırmaz
-                        forEach(t -> System.out.print(t + " "));
+                map(Math::sqrt). // Math class indan sqrt() method call edildi.
+//                forEach(Lambda01::yazdir); // yazdir() metdo int. calistigi icin map den cikan datalari calistirmaz
+                forEach(t->System.out.print(t+" "));
     }
-    // Task : list'in en buyuk elemanini yazdiriniz
 
-    public static void maxElBul(List<Integer> sayi) {
+    // Task : list'in en buyuk elemanini yazdiriniz
+    public static void maxElmBul(List<Integer> sayi) {
         Optional<Integer> maxSayi = sayi.
                 stream().
-                reduce(Math::max);//akısa giren elemnalrın action sonrası tek eleman haline getirir
+                reduce(Math::max); // akisa giren elemanlarin action sonrasi tek eleman haline getiri
+                                    // reduce() den sonra terminal methodu forEach() gelmediginden atama yapip yazdirabiliriz. yada sout icinde yazabiliriz
 
-        System.out.println(maxSayi);// 15
-        System.out.println("halukca :" + sayi.stream().reduce(Math::max));//halukca : 15
+        System.out.println(maxSayi);
+        // Yada
+        System.out.println("sout ici : "+sayi.stream().reduce(Math::max));
         /*
-        reduce()-->azaltmak ... bir cok datayi tek bir dataya(max min carp top vs islemlerde) cevirmek icin kullanilir.
-        kullanımı yaygındır pratiktir.
-        Bir Stream içerisindeki verilerin teker teker işlenmesidir. Teker teker işleme sürecinde, bir önceki adımda elde edilen sonuç
-        bir sonraki adıma girdi olarak sunulmaktadır. Bu sayede yığılmlı bir hesaplama süreci elde edilmiş olmaktadır.
-        reduce metodu ilk parametrede identity değeri, ikinci parametrede ise BinaryOperator türünden bir obj kullanılır.
-        reduce işleminde bir önceki hesaplanmış değer ile sıradaki değer bir işleme tabi tutulmaktadır.
-        İşleme başlarken bir önceki değer olmadığı için bu değer identity parametresinde tanımlanmaktadır.
+         reduce()-->azaltmak ... bir cok datayi tek bir dataya(max min carp top vs islemlerde)
+         cevirmek icin kullanilir.
+         kullanımı yaygındır pratiktir.
+         Bir Stream içerisindeki verilerin teker teker işlenmesidir.
+         Teker teker işleme sürecinde, bir önceki adımda elde edilen sonuç bir sonraki adıma
+         girdi olarak sunulmaktadır. Bu sayede yığılmlı bir hesaplama süreci elde edilmiş olmaktadır.
+         reduce metodu ilk parametrede identity değeri, ikinci parametrede ise
+         BinaryOperator türünden bir obj kullanılır.
+         reduce işleminde bir önceki hesaplanmış değer ile sıradaki değer
+         bir işleme tabi tutulmaktadır.
+         İşleme başlarken bir önceki değer olmadığı için bu değer identity parametresinde tanımlanmaktadır.
 
-        */
+         */
     }
 
     // Task : List'in cift elemanlarin karelerinin en buyugunu print ediniz
-    public static void ciftKarteMaxBul(List<Integer> sayi) {
-
+    public static void ciftKareMaxBul(List<Integer> sayi) {
         System.out.println(sayi.
                 stream().
                 filter(Lambda01::ciftBul).
                 map(t -> t * t).
-                reduce(Math::max));//36
-
-        System.out.println("daha hızlı specific ınteger class : " + sayi.
+                reduce(Math::max));
+        System.out.println(sayi.
                 stream().
                 filter(Lambda01::ciftBul).
                 map(t -> t * t).
-                reduce(Integer::max));//36 specific class daha hızlı run olur
+                reduce(Integer::max)); // specific class daha hizli rum olur
     }
 
     // Task : List'teki tum elemanlarin toplamini yazdiriniz.
     //Lambda Expression...
-    public static void elTopla(List<Integer> sayi) {
-        int toplam = sayi.stream().reduce(0, (a, b) -> a + b);//Lambda Expression...
+    public static void elmTopla(List<Integer> sayi) {
+        int toplam = sayi.stream().reduce(0,(a,b)->a+b); // Lambda Expression...
         /*
         a ilk degerini her zaman atanan degerden (identity) alır
         b degerini her zamana  stream()'dan akısdan alır
         a ilk degerinden sonraki her değeri action(işlem)'dan alır
+       */
 
-               */
-        System.out.println("Lambda exp. : " + toplam);
-        //Method Ref...
+        System.out.println("Lambda Exp... : "+toplam);
+
+        // Method Ref...
         Optional<Integer> topla = sayi.stream().reduce(Integer::sum);
-        System.out.println("method ref :" + sayi.stream().reduce(Integer::sum));//method ref
-
+        System.out.println("Method Ref... : "+topla); // Method Ref...
+        // YADA
+        System.out.println("Method Ref... : "+sayi.stream().reduce(Integer::sum)); // Method Ref...
     }
-    // Task : List'teki cift elemanlarin carpimini  yazdiriniz.
 
+    // Task : List'teki cift elemanlarin carpimini  yazdiriniz.
     public static void ciftCarp(List<Integer> sayi) {
-//Method Ref...
-        System.out.println("method ref :" + sayi.
+        // Method Referance ... ile
+        System.out.println(sayi.
                 stream().
                 filter(Lambda01::ciftBul).
                 reduce(Math::multiplyExact));
-//Lambda Expression...
 
-        System.out.println("Lambda exp. : " + sayi.
+        //Lambda Expression... ile
+        System.out.println(sayi.
                 stream().
                 filter(Lambda01::ciftBul).
                 reduce(1, (a, b) -> (a * b)));
+
     }
 
     // Task : List'teki elemanlardan en kucugunu 4 farklı yontem ile print ediniz.
     public static void minBul(List<Integer> sayi) {
         //1. yontem Method Reference --> Integer class
         Optional<Integer> minSayiInteger = sayi.stream().reduce(Integer::min);
-        System.out.println(minSayiInteger);
+        System.out.println("minSayiInteger = " + minSayiInteger);
         //2. yontem Method Reference --> Math class
         Optional<Integer> minSayiMath = sayi.stream().reduce(Math::min);
-        System.out.println(minSayiMath);
+        System.out.println("minSayiMath = " + minSayiMath);
         //3. yontem Lambda Expression
-        int minSayiLJambda = (sayi.stream().reduce(Integer.MAX_VALUE, (x, y) -> x < y ? x : y));
-        System.out.println(minSayiLJambda);
+        int minSayiLJambda = sayi.stream().reduce(Integer.MAX_VALUE, (x,y) -> x<y ? x:y);
+        System.out.println("minSayiLJambda = " + minSayiLJambda);
         //4. yontem Method Reference --> Haluk class
-        Optional<Integer> minSayiHaluk = sayi.stream().reduce(Lambda02::byHalukMin);
-        System.out.println(minSayiHaluk);
+        Optional<Integer> minSayiMethodRef = sayi.stream().reduce(Lambda02::byMethodMin);
+        System.out.println("minSayiMethodRef = " + minSayiMethodRef);
     }
-
-    public static int byHalukMin(int a, int b) {//bu method kendisine verilen iki int degerin en kücügunu return eder
-        return a < b ? a : b;
+    private static int byMethodMin(int a, int b) {
+        return a<b ? a:b;
     }
 
     // Task : List'teki 5'ten buyuk en kucuk tek sayiyi print ediniz.
     public static void bestenBykEnKck(List<Integer> sayi) {
-
-        System.out.println(sayi.stream().filter(t -> t > 5 && t % 2 == 1).reduce(Lambda02::byHalukMin));
+        System.out.println(sayi.stream().filter(t -> t > 5 && t % 2 == 1).reduce(Lambda02::byMethodMin));
     }
 
     // Task : list'in cift  elemanlarinin karelerini  kucukten buyuge print ediniz.
-    public static void ciftKareKbPrint(List<Integer> sayi) {
+    public static void ciftKareKcktenBygePrint(List<Integer> sayi) {
         sayi.
-                stream().//akısa alındı
-                filter(Lambda01::ciftBul).//cift elemnlar fitrlenedi
-                map(t->t*t).//fitrelenen cift sayı karesi alındı
-                sorted().//karesi alınan elemanlar dogal(k->b) sıralandı
-                forEach(Lambda01::yazdir);//print edildi
+                stream().
+                filter(Lambda01::ciftBul).
+                map(t -> t*t).
+                sorted(). // karesi alinan sayilari dogal siraladi (kucukten buyuge)
+                forEach(Lambda01::yazdir);
 
         //sorted() => Doğal düzene göre sıralanmış, bu akışın elemanlarında oluşan bir akış döndürür.
         //Sorted() methodu tekrarlı kullanılırsa en son kullanılan aktif olur.
     }
 
     // Task : list'in tek  elemanlarinin kareleri ni buykten kucuge  print ediniz.
-
-    public static void tekKareBkPrint(List<Integer> sayi) {
-        sayi.//akıs kaynagı
-                stream().//akısa alındı
-                filter(t->t%2!=0).//tek elemnlar fitrlenedi
-                map(t->t*t).//fitrelenen cift sayı karesi alındı
-                sorted(Comparator.reverseOrder()).//karesi alınan elemanlar ters(b->k) sıralandı
-                forEach(Lambda01::yazdir);//print edildi
-
-
+    public static void tekKareByktenKcgePrint(List<Integer> sayi) {
+        sayi.
+                stream().
+                filter(t -> t%2!=0).
+                map(t -> t*t).
+                sorted(Comparator.reverseOrder()). // karesi alinan sayilari ters siraladi (buyukten kucuge)
+                forEach(Lambda01::yazdir);
     }
+
 }
--- src/lambdaTutorial/Lambda03.java
+++ src/lambdaTutorial/Lambda03.java
@@ -1,25 +1,184 @@
 package lambdaTutorial;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Lambda03 {
     public static void main(String[] args) {
 
+        List<String> menu=new ArrayList<>(Arrays.asList("kusleme","adana","trilece",
+                "havucDilim","buryan", "yaglama","kokarec","arabAsi","guvec","trilece","trilece","trilece", "waffle", "welwmwn", "cacix"));
 
-        System.out.println("\n   ***   ");
+
+        System.out.println("   *** alfBykHarfTkrsz() ***   ");
+        alfBykHarfTkrsz(menu); //1156 484 256 400 64 1444
+        System.out.println("\n\n   *** chrSayisiTersSirali() ***   ");
+        chrSayisiTersSirali(menu);
+        System.out.println("\n\n   *** chrSayisiKcktenBykSirali() ***   ");
+        chrSayisiKcktenBykSirali(menu);
+        System.out.println("\n\n   *** sonHarfByktenSirali() ***   ");
+        sonHarfByktenSirali(menu);
+        System.out.println("\n\n   *** charKaresiCiftElSirali() ***   ");
+        charKaresiCiftElSirali(menu);
+        System.out.println("\n\n   *** harfSayisi7denAzKontrol() ***   ");
+        harfSayisi7denAzKontrol(menu);
+        System.out.println("\n\n   *** wIleBaslayanElmKontrol() ***   ");
+        wIleBaslayanElmKontrol(menu);
+        System.out.println("\n\n   *** xIleBitenElmKontrol() ***   ");
+        xIleBitenElmKontrol(menu);
+        System.out.println("\n\n   *** charSayisiBykElmPrint() ***   ");
+        charSayisiBykElmPrint(menu);
+        System.out.println("\n\n   *** ilkElmHaricSonHrfSiraliPrint() ***   ");
+        ilkElmHaricSonHrfSiraliPrint(menu);
+
+
+
     }
+
     // Task : List elemanlarini alafabetik buyuk harf ve  tekrarsiz print ediniz.
+    public static void alfBykHarfTkrsz (List<String> yemek){
+        yemek.stream().
+                //map(t->t.toUpperCase()). //
+                map(String::toUpperCase).
+                sorted(). // siraladi
+                distinct(). // benzersiz (tekrarsiz) hale getirdi
+                forEach(t->System.out.print(t+" "));
+
+        // distinct() => Bu method tekrarlı elemanları sadece bir kere yazdırır.
+        // Bu akışın farklı elemanlarından (Object.equals (Object) 'e göre) oluşan bir akış döndürür.
+        // Sıralı akışlar için, farklı elemanın seçimi sabittir
+        // (yinelenen öğeler için, karşılaşma sırasında ilk görünen öğe korunur.)
+        // Sırasız akışlar için, herhangi bir kararlılık garantisi verilmez. Stream return eder.
+    }
 
     // Task : list elelmanlarinin character sayisini ters sirali olarak tekrarsiz print ediniz..
+    public static void chrSayisiTersSirali (List<String> menu){
+        menu.
+                stream(). //akısa alındı
+                // map(t->t.length()).
+                map(String::length).//elemanlar karakter sayısına update edildi
+                sorted(Comparator.reverseOrder()).//ters sıra yapıldı
+                distinct().//benzersiz yapıldı
+                //forEach(t->System.out.print(t+" "));
+                forEach(Lambda01::yazdir);//print edildi, int oldugu icin calisir
+    }
 
     // Task : List elemanlarini character sayisina gore kckten byk e gore print ediniz..
+    public static void chrSayisiKcktenBykSirali (List<String> menu){
+        menu.
+                stream().
+                sorted(Comparator.
+                        comparing(String::length)).
+                forEach(t->System.out.print(t+" "));
+    }
+
     // Task : list elemanlarinin son harfine gore ters sirali print ediniz.
+    public static void sonHarfByktenSirali (List<String> menu){
+        menu.
+                stream().
+                sorted(Comparator.
+                        comparing(t-> t.toString().
+                                charAt(t.toString().length()-1)).
+                        reversed()).
+                forEach(t->System.out.print(t+" "));
+    }
+
     // Task : listin elemanlarin karakterlerinin cift sayili  karelerini hesaplayan,ve karelerini tekrarsiz buyukten kucuge sirali  print ediniz..
+    public static void charKaresiCiftElSirali (List<String> menu){
+        menu.
+                stream().//akısa alndı
+                map(t -> t.length() * t.length()).//akısdaki string elemanları boyutlarının karesine update edildi
+                filter(Lambda01::ciftBul).//cift elelmalar filtrelendi
+                distinct().//tekrarsız yapıldı
+                sorted(Comparator.reverseOrder()).//ters b->k sıra yapıldı
+                forEach(Lambda01::yazdir);// print edildi
+    }
+
+
     // Task : List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz.
+    public static void harfSayisi7denAzKontrol (List<String> menu){
+        // amele code :)
+        System.out.println("amele code");
+        boolean kontrol = menu.stream().allMatch(t -> t.length() <= 7);
+        if (kontrol) {
+            System.out.println("list elemanları 7 ve daha az harften olusuyor");
+        } else System.out.println("list elemanları 7 harften  buyuk");
+        // cincik code :)
+        System.out.println("cincix code");
+        System.out.println(menu.
+                stream().
+                allMatch(t -> t.length() <= 7) ? "list elemanları 7 ve daha az harften olusuyor" :
+                "list elemanları 7 harften  buyuk");
+
+        //anyMatch() --> enaz bir eleman sarti saglarsa true aksi durumda false return eder
+        //allMatch() --> tum  elemanlar sarti saglarsa true en az bir eleman sarti saglamazsa false return eder.
+        //noneMatch() --> hic bir sarti SAGLAMAZSA true en az bir eleman sarti SAGLARSA false return eder.
+    }
+
     // Task : List elelmanlarinin "W" ile baslamasını kontrol ediniz.
+    public static void wIleBaslayanElmKontrol (List<String> menu){
+        System.out.println(menu.
+                stream().
+                noneMatch(t->t.startsWith("w")) ? "agam w ile baslayan yemahh olu mu ?" :
+                "agam wenemen ne menen bi sey ?");
+    }
+
     // Task : List elelmanlarinin "x" ile biten en az bir elemaı kontrol ediniz.
+    public static void xIleBitenElmKontrol (List<String> menu){
+        System.out.println(menu.
+                stream().
+                anyMatch(t -> t.endsWith("x")) ? "agam senden bir  cacix olmaz  ?" :
+                "agam senin aradigin yemek bu torpaklarda yooogggg");
+    }
+
     // Task : Karakter sayisi en buyuk elemani yazdiriniz.
+    public static void charSayisiBykElmPrint (List<String> menu){
+        System.out.println(menu.
+                stream().
+                sorted(Comparator.comparing(t -> t.toString().length()).
+                        reversed()).
+                findFirst()); // ilk eleman alindi
+
+
+        // akıs cıktısını bir veriable assaign edilebilir
+
+        Optional<String> enBykKrEl= menu.
+                stream().
+                sorted(Comparator.comparing(t -> t.toString().length()).
+                        reversed()).
+                findFirst(); // ilk eleman alindi
+        System.out.println(enBykKrEl);
+
+        //veya
+        Stream<String> sonIsim = menu.
+                stream().
+                sorted(Comparator.comparing(t -> t.toString().length()).
+                        reversed()).
+                // findFirst() yerine asagidaki gibi
+                limit(1);//limit(a) akısdan cıkan elemanları a parametresine gore ilk a elamanı alır.
+
+        // sonIsim.toArray()--> limit() meth return dan dolayı  stream type olan sonIsim toArray() method ile array type convert edildi
+        System.out.println(Arrays.toString(sonIsim.toArray()));//arraya cevrilen sonIsim stream print edildi
+ /*
+  TRİCK : Stream'ler ekrana direk yazdırılamaz. Stream'i toArray() ile Array'e çeviririz. Array'i de Arrays.toString() 'in içine alıp yazdırabiliriz.
+•  Ör; System.out.println(Arrays.toString(stream.toArray())); veya System.out.println(Arrays.asList(***.toArray())); kullanılabilir.
+   */
+
+        //limit(1) => Sınırlandırma demek. Bu akışın elemanlarından oluşan, uzunluğu maxSize'dan uzun olmayacak
+        // şekilde kesilmiş bir akış return eder. Stream return eder.
+    }
+
     // Task : list elemanlarini son harfine göre siralayıp ilk eleman hariç kalan elemanlari print ediniz.
+    public static void ilkElmHaricSonHrfSiraliPrint (List<String> menu){
+        menu.
+                stream().
+                sorted(Comparator.comparing(t->t.charAt(t.length()-1))). //son harfe gore siralandi
+                skip(1). //ilk eleman atlandı -->adana
+                forEach(t->System.out.print(t+" "));
 
-
-
+        //skip(1) => atlama demek. Akışın ilk n elemanını attıktan sonra bu akışın kalan elemanlarından oluşan bir akış return eder.
+        // Bu akış n'den daha az öğe içeriyorsa, boş bir akış döndürülür. Bu, durum bilgisi olan bir ara işlemdir.
+        //skip(list.size()-1) => List'in uzunluğunun 1 eksiğini yazarsak son elemanı yazdırırız.
+    }
 
 }
